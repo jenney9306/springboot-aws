7a21353674521cf061827da6a27cb4d639c63724
jenney9306 <7636290@naver.com>
2022-04-29 23:37:00 +0900

    add 게시글 등록 수정 삭제

--- src/main/java/com/jiyea/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jiyea/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,12 @@
 package com.jiyea.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+    // Springdatajpa 에서 제공되지 않은 메소드는 쿼리로 작성해도 됨
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/jiyea/springboot/web/PostsApiController.java
+++ src/main/java/com/jiyea/springboot/web/PostsApiController.java
@@ -16,6 +16,7 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
     }
+
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
         return postsService.update(id, requestDto);
@@ -23,5 +24,11 @@ public class PostsApiController {
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id){
         return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
     }
 }
